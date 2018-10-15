165910e4b3a30fa8fb9509f021efa94789d4435a
Nakmak98 <nakmak1998@gmail.com>
2018-10-16 02:45:23 +0300

    Fix front for web-server

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php ?>
+	<div class="mobile-menu">
+		<div id="close" class="burger">
+			<i class="fas fa-bars fa-2x"></i>
+		</div>
+		студент года <span>2018</span>
+		<ul id="menu-items">
+			<li><a href="#">номинации</a></li>
+			<li><a href="#">положение</a></li>
+			<li><a href="#">подать заявку</a></li>
+			<li><a href="#">победители</a></li>
+			<li><a href="#">контакты</a></li>
+		</ul>
+	</div>
+	<header>
+		<div id="open" class="burger">
+			<i class="fas fa-bars fa-2x"></i>
+		</div>
+		<p><a href="home.php">студент года <span>2018</span></a></p>
+		<div class="header-divide1"></div>
+		<div class="nav-bar">
+			<a href="#">номинации</a>
+			<a href="#">положение</a>
+			<a href="#">подать заявку</a>
+			<a href="#">победители</a>
+			<a href="#">контакты</a>
+		</div>
+		<div class="header-divide"></div>
+		</div>
+	</header>
