3ad4f5c5cb727f01e0034700efbfc5dd03a96a8b
Nakmak98 <nakmak1998@gmail.com>
2018-10-17 01:15:51 +0300

    refactor home page

--- home.php
+++ home.php
@@ -10,41 +10,14 @@
 	<title>Base</title>
 </head>
 <body>
-	<div class="mobile-menu">
-		<div id="close" class="burger">
-			<i class="fas fa-bars fa-2x"></i>
-		</div>
-		студент года <span> 2018</span>
-		<ul id="menu-items">
-			<li><a href="#">номинации</a></li>
-			<li><a href="#">положение</a></li>
-			<li><a href="#">подать заявку</a></li>
-			<li><a href="#">победители</a></li>
-			<li><a href="#">контакты</a></li>
-		</ul>
-	</div>
-	<header>
-
-		<div id="open" class="burger">
-			<i class="fas fa-bars fa-2x"></i>
-		</div>
-		<p class="home-header-title">студент года <span> 2018</span></p>
-		<div class="home-nav-bar">
-			<a href="#">номинации</a>
-			<a href="#">положение</a>
-			<a href="#">подать заявку</a>
-			<a href="#">победители</a>
-			<a href="#">контакты</a>
-		</div>
-		<div class="home-header-divide"></div>
-	</header>
+	<?php include "home-header.php";?> 
 	<main>
 		<div class="home-container">
 			<img class="student_desktop"src="img/student.png" alt="student">
 		</div>
 	</main>
 	<footer>
-		<p>МГТУ им. Н.Э. Баумана <br> 2018<p> 
+		<p>МГТУ им. Н.Э. Баумана <br>2018<p> 
 	</footer>
 </body>
 </html>
