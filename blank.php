3c61eb27f304c9460738b6710f219c14a599a440
koolana <kolya_andreychik@mail.ru>
2018-10-17 19:39:57 +0300

    php-files

---
+++ blank.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/base.js" defer></script>
+	<title>Заявки</title>
+</head>
+<body>
+	<?php include "header.php";?> 
+
+	<main>
+		<div class="application_block_head">
+			<p>Подать заявку</p>
+		</div>
+
+		<div class="blank_container_center">
+			<div class="blank_container">
+				<div class="blank_blackBox">
+					123
+				</div>
+
+				<div class="blank_container_blackCircle">
+					<div class="blackCircle"></div>
+					<div class="blackText">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+				<div class="blank_container_blackCircle">
+					<div class="blackCircle"></div>
+					<div class="blackText">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+				<div class="blank_container_blackCircle">
+					<div class="blackCircle"></div>
+					<div class="blackText">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="application_block_button">
+			<div class="application_button">
+				<p>Бланк<br>эссе</p>
+			</div>
+
+			<div class="application_button">
+				<p>Скачать анкету</p>
+			</div>
+		</div>
+		<!--  -->
+	</main>
+	<footer>
+		<p>МГТУ им. Н.Э. Баумана <br> 2018<p> 
+
+	</footer>
+</body>
+</html>
---
+++ winners.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/base.js" defer></script>
+	<title>Победители</title>
+</head>
+<body>
+	<?php include "header.php";?> 
+
+	<main>
+		<div class="application_block_head">
+			<p>Победители</p>
+		</div>
+
+		<div class="blank_container_center">
+			<div class="blank_container">
+				<div class="winYear_buttons">
+					<div class="button">
+						2017
+					</div>
+
+					<div class="button">
+						2016		
+					</div>
+
+					<div class="button">
+						2015
+					</div>
+				</div>
+
+				<div class="oneWinner_container">
+					<div class="photoWinner">
+						<img src="img/contacts-photo.png">
+					</div>
+
+					<div class="textWinner_container">
+						<div class="textWinner_head">
+							<h>Иван Иванов</h>
+							<div class = "greenBox">
+								Спортсмен
+							</div>
+						</div>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet porttitor eget dolor morbi non arcu risus. Vestibulum lectus mauris ultrices eros in. Nunc mi ipsum faucibus vitae aliquet. Ornare suspendisse sed nisi lacus sed viverra tellus. Adipiscing commodo elit at imperdiet dui accumsan. Mattis pellentesque id nibh tortor. Commodo nulla facilisi nullam vehicula ipsum a arcu. Risus in hendrerit gravida rutrum quisque non. Ut consequat semper viverra nam libero justo laoreet. Sit amet venenatis urna cursus eget nunc scelerisque. Velit scelerisque in dictum non consectetur a erat nam. Diam phasellus vestibulum lorem sed risus ultricies. Amet aliquam id diam maecenas ultricies mi eget. Mauris commodo quis imperdiet massa. Eget duis at tellus at.
+
+						<div class="prize">
+							<h>Приз:</h>
+							<p>что-то</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="oneWinner_container">
+					<div class="photoWinner">
+						<img src="img/contacts-photo.png">
+					</div>
+
+					<div class="textWinner_container">
+						<div class="textWinner_head">
+							<h>Иван Иванов</h>
+							<div class = "greenBox">
+								Спортсмен
+							</div>
+						</div>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet porttitor eget dolor morbi non arcu risus. Vestibulum lectus mauris ultrices eros in. Nunc mi ipsum faucibus vitae aliquet. Ornare suspendisse sed nisi lacus sed viverra tellus. Adipiscing commodo elit at imperdiet dui accumsan. Mattis pellentesque id nibh tortor. Commodo nulla facilisi nullam vehicula ipsum a arcu. Risus in hendrerit gravida rutrum quisque non. Ut consequat semper viverra nam libero justo laoreet. Sit amet venenatis urna cursus eget nunc scelerisque. Velit scelerisque in dictum non consectetur a erat nam. Diam phasellus vestibulum lorem sed risus ultricies. Amet aliquam id diam maecenas ultricies mi eget. Mauris commodo quis imperdiet massa. Eget duis at tellus at.
+
+						<div class="prize">
+							<h>Приз:</h>
+							<p>что-то</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="oneWinner_container">
+					<div class="photoWinner">
+						<img src="img/contacts-photo.png">
+					</div>
+
+					<div class="textWinner_container">
+						<div class="textWinner_head">
+							<h>Иван Иванов</h>
+							<div class = "greenBox">
+								Спортсмен
+							</div>
+						</div>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet porttitor eget dolor morbi non arcu risus. Vestibulum lectus mauris ultrices eros in. Nunc mi ipsum faucibus vitae aliquet. Ornare suspendisse sed nisi lacus sed viverra tellus. Adipiscing commodo elit at imperdiet dui accumsan. Mattis pellentesque id nibh tortor. Commodo nulla facilisi nullam vehicula ipsum a arcu. Risus in hendrerit gravida rutrum quisque non. Ut consequat semper viverra nam libero justo laoreet. Sit amet venenatis urna cursus eget nunc scelerisque. Velit scelerisque in dictum non consectetur a erat nam. Diam phasellus vestibulum lorem sed risus ultricies. Amet aliquam id diam maecenas ultricies mi eget. Mauris commodo quis imperdiet massa. Eget duis at tellus at.
+
+						<div class="prize">
+							<h>Приз:</h>
+							<p>что-то</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="oneWinner_container">
+					<div class="photoWinner">
+						<img src="img/contacts-photo.png">
+					</div>
+
+					<div class="textWinner_container">
+						<div class="textWinner_head">
+							<h>Иван Иванов</h>
+							<div class = "greenBox">
+								Спортсмен
+							</div>
+						</div>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet porttitor eget dolor morbi non arcu risus. Vestibulum lectus mauris ultrices eros in. Nunc mi ipsum faucibus vitae aliquet. Ornare suspendisse sed nisi lacus sed viverra tellus. Adipiscing commodo elit at imperdiet dui accumsan. Mattis pellentesque id nibh tortor. Commodo nulla facilisi nullam vehicula ipsum a arcu. Risus in hendrerit gravida rutrum quisque non. Ut consequat semper viverra nam libero justo laoreet. Sit amet venenatis urna cursus eget nunc scelerisque. Velit scelerisque in dictum non consectetur a erat nam. Diam phasellus vestibulum lorem sed risus ultricies. Amet aliquam id diam maecenas ultricies mi eget. Mauris commodo quis imperdiet massa. Eget duis at tellus at.
+
+						<div class="prize">
+							<h>Приз:</h>
+							<p>что-то</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="oneWinner_container">
+					<div class="photoWinner">
+						<img src="img/contacts-photo.png">
+					</div>
+
+					<div class="textWinner_container">
+						<div class="textWinner_head">
+							<h>Иван Иванов</h>
+							<div class = "greenBox">
+								Спортсмен
+							</div>
+						</div>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet porttitor eget dolor morbi non arcu risus. Vestibulum lectus mauris ultrices eros in. Nunc mi ipsum faucibus vitae aliquet. Ornare suspendisse sed nisi lacus sed viverra tellus. Adipiscing commodo elit at imperdiet dui accumsan. Mattis pellentesque id nibh tortor. Commodo nulla facilisi nullam vehicula ipsum a arcu. Risus in hendrerit gravida rutrum quisque non. Ut consequat semper viverra nam libero justo laoreet. Sit amet venenatis urna cursus eget nunc scelerisque. Velit scelerisque in dictum non consectetur a erat nam. Diam phasellus vestibulum lorem sed risus ultricies. Amet aliquam id diam maecenas ultricies mi eget. Mauris commodo quis imperdiet massa. Eget duis at tellus at.
+
+						<div class="prize">
+							<h>Приз:</h>
+							<p>что-то</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--  -->
+	</main>
+	<footer>
+		<p>МГТУ им. Н.Э. Баумана <br> 2018<p> 
+
+	</footer>
+</body>
+</html>
